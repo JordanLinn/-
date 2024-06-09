62652f7de375d31cee7d82c0fac0cc6dcd78cb6a
JordanLinn <155252468+JordanLinn@users.noreply.github.com>
2024-06-09 23:49:23 +0800

    Update aboutlms.php

---
+++ aboutlms.php
@@ -0,0 +1,80 @@
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=gb2312" />
+<link rel="shortcut icon" href="favicon.ico" />
+<title>����LMS</title>
+<link href="lms.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<!--***************************header***************************-->
+<?php include "data/header.inc"?>
+<!--***************************main**************************-->
+<center>
+<table border="1" width="800" height="30" cellspacing="0" cellpadding="0" bgcolor="#E4E4E4" bordercolorlight="#C2C2C2" bordercolordark="#FFFFFF">
+	<tr>
+  	<td width="234" align="center">
+  	<img src="pics/position.png" alt="position" width="16" height="16" align="absmiddle">&nbsp;���λ��</td>
+    <td><img src="pics/rightdir.png" alt="rightdir" width="16" height="16" align="absmiddle">[&gt;<a href="aboutlms.php">����LMS</a>&lt;]</td>
+	</tr>
+</table>
+</center>
+
+<center>
+<table border="1" width="800" cellspacing="0" cellpadding="0" bgcolor="#E4E4E4" bordercolorlight="#C2C2C2" bordercolordark="#FFFFFF">
+    <tr>
+	<td>
+	&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	<img src="pics/LMS.png" alt="����LMS" width="50" height="50" align="absmiddle">
+	<strong><font color="#66CCFF" size="10">����LMS</font></strong>	</td>
+	</tr>
+</table>
+</center>
+
+<center>
+<table border="1" width="800" cellspacing="0" cellpadding="0" bgcolor="#E4E4E4" bordercolorlight="#C2C2C2" bordercolordark="#FFFFFF">
+    <tr align="center">
+	<td colspan="2">
+	  <font color="#FF6600"><p>LMS���</p></font></td>
+	</tr>
+	<tr>
+	<td colspan="2">
+    <p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	LMS��ΪLibraryManageSystem����д����ϵͳӵ��ͼ����������ŷ������û������ȹ��ܡ�ʹ�ñ�ϵͳ����Ҫ����PHP&amp;MySQL�������������ԣ�ϵͳ��windowsXP SP2������������</p>
+	<p>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	LMS����С����Ա����:</p>	</tr>
+	<tr>
+	<td width="15%" align="center">
+	  <a href="lqws/index.html">����ǿ</a></td>
+	<td>�鳤������ƣ������д</td>
+	</tr>
+	<tr>
+	  <td width="15%" align="center">��&nbsp;&nbsp;&nbsp;&nbsp;��</td>
+      <td>���������PHP�����д������</td>
+	</tr>
+	<tr>
+	  <td width="15%" align="center">л&nbsp;&nbsp;&nbsp;&nbsp;��</td>
+      <td>�߼��ṹ��ƣ�PHP�����д������</td>
+	</tr>
+	<tr>
+	  <td width="15%" align="center">��Ф��</td>
+      <td>���ݿ����к�ά����PHP�����д������</td>
+	</tr>
+	<tr>
+	  <td width="15%" align="center">������</td>
+      <td>���ݿ����к�ά��</td>
+	</tr>
+	<tr>
+	  <td width="15%" align="center">��&nbsp;&nbsp;&nbsp;&nbsp;��</td>
+      <td>���E-Rͼ</td>
+	</tr>
+	<tr>
+	  <td colspan="2">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;      </td>
+    </tr>
+	<tr>
+</table>
+</center>
+<!--***************************footer***************************-->
+<?php include "data/footer.inc"?>
+</body>
+</html>
