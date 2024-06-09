59c239ea2759e1670b042c74df7d598f845aaefa
JordanLinn <155252468+JordanLinn@users.noreply.github.com>
2024-06-09 23:58:09 +0800

    Update success.php

---
+++ success.php
@@ -0,0 +1,39 @@
+<html>
+<head>
+<link rel="shortcut icon" href="favicon.ico" />
+<title>�ɹ�</title>
+</head>
+<body>
+<br><br><br><br><br>
+<center>
+<img src="pics/success.png" alt="success" width="128" height="128">
+</center>
+<center>
+<strong>
+<font size="+2" color="#7ABB28">
+<!--*******************error*******************-->
+success
+<br>
+<?php 
+	
+	//echo "<META HTTP-EQUIV=\"Refresh\" Content=\"2;URL = ".$nextpage."?upmd5=".$upmd5."&login=".$loginname."\">";
+	//echo "<meta http-equiv=\"Pragram\"content=\"no-cache\">";
+	//$nextpage =
+	//$successno =
+	
+	echo "<META HTTP-EQUIV=\"Refresh\" Content=\"2;URL = ".$nextpage."\">";
+		echo "<meta http-equiv=\"Pragram\"content=\"no-cache\">";
+	if($successno==2){	echo "ɾ���û��ɹ�!";}
+	if($successno==3){	echo "�޸�ͼ��ɹ���";}
+	if($successno==4){	echo "����ͼ��ɹ���";}
+	if($successno==5){	echo "ɾ��ͼ��ɹ���";}
+	if($successno==6){	echo "����ɹ�!";}
+	if($successno==7){	echo "����ɹ�!";}
+	if($successno==8){	echo " �����û��ɹ���";}
+
+?>
+
+</font></strong>
+</center>
+</body>
+</html>
